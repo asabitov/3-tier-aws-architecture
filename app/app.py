9825adc26cac7bdd90d024b72d6a53af1fa35e26
root <root@ip-10-0-3-36.ap-southeast-2.compute.internal>
2019-12-10 01:19:32 +0000

    changes

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def app_msg:
+def app_msg():
     return 'Hi, the app is running OK!'
 
 
-if __name == '__main__':
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
 
